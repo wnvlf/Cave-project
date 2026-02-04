716f4afe54f197e8f16d7d5cdd7be5ca5223b998
wnvlf <139468663+wnvlf@users.noreply.github.com>
2026-02-04 19:33:50 +0900

    feat: 오디오 매니저 개발 및 환경 설정 개발

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("#BGM")]
+    public AudioClip[] bgmClip;
+    public float bgmVolume;
+    public bool bgmVolumeMute;
+    AudioSource bgmPlayer;
+    AudioHighPassFilter bgmEffect;
+    public Bgm bgm;
+
+    public enum Bgm { title, Home, Shop, Battle, None }
+
+    [Header("#SFX")]
+    public AudioClip[] sfxClips;
+    public float sfxVolume;
+    public bool sfxVolumeMute;
+    public int channels;
+    AudioSource[] sfxPlayer;
+    int channelIndex;
+
+    public enum Sfx { Click ,Sword ,Masic }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            Init();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void Init()
+    {
+        // 배경음 플레이어 초기화
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.playOnAwake = false;
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.clip = bgmClip[0];
+        bgmPlayer.loop = true;
+        bgmEffect = Camera.main.GetComponent<AudioHighPassFilter>();
+
+        //효과음 플레이어 초기화
+        GameObject sfxObject = new GameObject("SfxPlayer");
+        sfxObject.transform.parent = transform;
+        sfxPlayer = new AudioSource[channels];
+
+        for (int index = 0; index < channels; index++)
+        {
+            sfxPlayer[index] = sfxObject.AddComponent<AudioSource>();
+            sfxPlayer[index].playOnAwake = false;
+            sfxPlayer[index].bypassListenerEffects = true;
+            sfxPlayer[index].volume = sfxVolume;
+        }
+    }
+
+    public void PlayBgm(Bgm bgm, bool isPlay)
+    {
+        if (this.bgm == bgm)
+            return;
+
+        if (isPlay)
+        {
+            this.bgm = bgm;
+            bgmPlayer.clip = bgmClip[(int)bgm];
+            bgmPlayer.Play();
+        }
+        else
+        {
+            bgmPlayer.Stop();
+        }
+    }
+
+    public void EffectBgm(bool isPlay)
+    {
+        bgmEffect.enabled = isPlay;
+    }
+
+    public void PlaySfx(Sfx sfx)
+    {
+        for (int index = 0; index < channels; index++)
+        {
+            int loopIndex = (index + channelIndex) % sfxPlayer.Length;
+
+            if (sfxPlayer[loopIndex].isPlaying)
+            {
+                continue;
+            }
+
+            int ranIndex = 0;
+
+            channelIndex = loopIndex;
+            sfxPlayer[loopIndex].clip = sfxClips[(int)sfx + ranIndex];
+            sfxPlayer[loopIndex].Play();
+            break;
+        }
+
+    }
+
+    public void SetBgmVolume(float volume)
+    {
+        bgmPlayer.volume = volume;
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        for (int i = 0; i < channels; i++)
+        {
+            sfxPlayer[i].volume = volume;
+        }
+    }
+}
---
+++ Assets/Scripts/MainOption.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainOption : MonoBehaviour
+{
+    public GameObject UIWindow = null;
+
+    [Header("사운드")]
+    public GameObject bgmVolumeImage;
+    public GameObject bgmVolumeMuteImage;
+    public GameObject sfxVolumeImage;
+    public GameObject sfxVolumeMuteImage;
+
+    [Header("환경 설정 UI")]
+    public TMP_Dropdown IsFullscreen;
+    public TMP_Dropdown Resolution;
+    public Slider Music;
+    public Slider Sfx;
+
+    private void Start()
+    {
+        SettingsManager.instance.LoadSettings();
+        UpdateUI();
+    }
+
+    public void OpenUI()
+    {
+        if (UIWindow)
+        {
+            UIWindow.SetActive(true);
+        }
+    }
+
+    public void CloseUI()
+    {
+        if (UIWindow)
+        {
+            Debug.Log("Close");
+            UIWindow.SetActive(false);
+        }
+    }
+
+    public void ChangeConfirm()
+    {
+        SettingsManager.instance.SaveSettings();
+    }
+
+    public void UpdateUI()
+    {
+        IsFullscreen.value = (PlayerPrefs.GetInt("IsFullscreen") == 1 ? 0 : 1);
+        Resolution.value = PlayerPrefs.GetInt("ResolutionIndex");
+        Music.value = PlayerPrefs.GetFloat("MusicVolume");
+        Sfx.value = PlayerPrefs.GetFloat("SfxVolume");
+    }
+
+    public void ChangeScreenMode(int index) // 화면 모드 전환
+    {
+        switch (index)
+        {
+            case 0:
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                SettingsManager.instance.IsFullScreen = true;
+                Debug.Log("전체화면");
+                break;
+            case 1:
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                SettingsManager.instance.IsFullScreen = false;
+                Debug.Log("창모드");
+                break;
+            default:
+                Debug.Log("Error");
+                break;
+        }
+    }
+
+    public void ChangeResolution(int index) // 화면 해상도 조절
+    {
+        switch (index)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, Screen.fullScreenMode);
+                SettingsManager.instance.ResolutionIndex = index;
+                break;
+            case 1:
+                Screen.SetResolution(1600, 900, Screen.fullScreenMode);
+                SettingsManager.instance.ResolutionIndex = index;
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, Screen.fullScreenMode);
+                SettingsManager.instance.ResolutionIndex = index;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetBgmVolume(float volume) // 배경음 조절
+    {
+        AudioManager.instance.SetBgmVolume(volume);
+        SettingsManager.instance.MusicVolume = volume;
+    }
+
+    public void SetSfxVolume(float volume) // 효과음 조절
+    {
+        AudioManager.instance.SetSfxVolume(volume);
+        SettingsManager.instance.SfxVolume = volume;
+    }
+
+    public void FBgmButton()
+    {
+        if (!AudioManager.instance.bgmVolumeMute) // 음소거 상태 아닐때
+        {
+            //bgmVolumeImage.SetActive(false);
+            //bgmVolumeMuteImage.SetActive(true);
+        }
+        else // 음소거 상태
+        {
+            //bgmVolumeImage.SetActive(true);
+            //bgmVolumeMuteImage.SetActive(false);
+        }
+        AudioManager.instance.bgmVolumeMute = !AudioManager.instance.bgmVolumeMute;
+        SetBgmVolume(AudioManager.instance.bgmVolume);
+    }
+
+    public void FsfxButton()
+    {
+        if (!AudioManager.instance.sfxVolumeMute) // 음소거 상태 아닐때
+        {
+            //sfxVolumeImage.SetActive(false);
+            //sfxVolumeMuteImage.SetActive(true);
+        }
+        else // 음소거 상태
+        {
+            //sfxVolumeImage.SetActive(true);
+            //sfxVolumeMuteImage.SetActive(false);
+        }
+        AudioManager.instance.sfxVolumeMute = !AudioManager.instance.sfxVolumeMute;
+        SetSfxVolume(AudioManager.instance.sfxVolume);
+    }
+
+    public void SaveSet()
+    {
+        SettingsManager.instance.SaveSettings();
+    }
+
+    public void LoadSet()
+    {
+        SettingsManager.instance.LoadSettings();
+        ChangeResolution(SettingsManager.instance.ResolutionIndex);
+        ChangeScreenMode(SettingsManager.instance.IsFullScreen ? 0 : 1);
+        UpdateUI();
+    }
+}
